decdfa31720598296747a8eac8b7de5dbf8de04a
maran <maara@makoitlab.com>
2025-08-02 21:48:22 +0530

    banner image change

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,11 @@ const App = () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img src="./hero.png" alt="Hero Banner" />
+            <img
+              src="./Hero_bg1.png"
+              className="w-full h-auto object-contain"
+              alt="Hero Banner"
+            />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without the Hassle
